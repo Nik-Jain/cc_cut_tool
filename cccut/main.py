07baf0417eca440d5a65cd9580411b0cef01d100
Nikhil Jain <nikhil-jain@hotmail.com>
2024-05-29 10:20:18 +0530

    Add unit test to test field option

--- cccut/main.py
+++ cccut/main.py
@@ -10,16 +10,18 @@ def _version_callback():
 
 @app.command()
 def main(
-    file: typer.FileText,
+    file: typer.FileText = typer.Argument(default=None),
     version: bool = typer.Option(False, '--version', '-v', help="Show the application's version and exit."),
-    fields: str = typer.Option(..., '-f', help='Fields to extract')
+    fields: str = typer.Option(None, '-f', help='Fields to extract')
     ) -> None:
     if version:
         _version_callback()
-    field_indices = [int(i) - 1 for i in fields]
-    for line in file:
-        parts = line.strip().split('\t')
-        selected_parts = [parts[i] for i in field_indices if i < len(parts)]
-        print('\t'.join(selected_parts))
+        return typer.Exit()
+    if fields:
+        field_indices = [int(i) - 1 for i in fields]
+        for line in file:
+            parts = line.strip().split('\t')
+            selected_parts = [parts[i] for i in field_indices if i < len(parts)]
+            print('\t'.join(selected_parts))
 if __name__ == 'main.py':
     app()
--- tests/test_cccut.py
+++ tests/test_cccut.py
@@ -1,4 +1,5 @@
 import subprocess
+import pytest
 
 def test_version():
     result = subprocess.run(['cccut', '--version'], capture_output=True, text=True)
@@ -6,4 +7,9 @@ def test_version():
     result = subprocess.run(['cccut', '-v'], capture_output=True, text=True)
     assert "cccut v0.1.0" in result.stdout
 
+@pytest.mark.parametrize("field_option, field_range, file_name, expected_result", 
+                         [("-f", "2", "sample.tsv", "f1\n1\n6\n11\n16\n21\n"),])
+def test_field_option(field_option, field_range, file_name, expected_result):
+    result = subprocess.run(['cccut', field_option, field_range, file_name], capture_output=True, text=True)
+    assert expected_result == result.stdout
 
