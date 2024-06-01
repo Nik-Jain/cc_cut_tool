5a1986854706d271413ab00a6697b4f2b5a7799d
Nikhil Jain <nikhil-jain@hotmail.com>
2024-06-01 05:54:12 +0530

    Enhance -f option to suport comma or whitespace separated list of fields

--- tests/test_cccut.py
+++ tests/test_cccut.py
@@ -9,13 +9,15 @@ def test_version():
     assert "cccut v0.1.0" in result.stdout
 
 @pytest.mark.parametrize("field_option, field_range, file_name, expected_result", 
-                         [("-f", "2", "sample.tsv", "f1\n1\n6\n11\n16\n21\n"),])
+                         [("-f", "2", "sample.tsv", "f1\n1\n6\n11\n16\n21\n"),
+                          ("-f", "1,2", "sample.tsv", 'f0\tf1\n0\t1\n5\t6\n10\t11\n15\t16\n20\t21\n'),])
 def test_field_option(field_option, field_range, file_name, expected_result):
     result = subprocess.run(['cccut', field_option, field_range, file_name], capture_output=True, text=True)
     assert expected_result == result.stdout
 
 @pytest.mark.parametrize("field_option, field_range, delimiter_option, delimiter, file_name, expected_result", 
-                         [("-f", "2", "-d", ",", "fourchords.csv", "Artist\nMatt Redman\xa0and\xa0Jonas Myrin\nThirsty Merc\nHarry Styles\nToto\nCheb Khaled\nMichel Teló\nMichelle Branch\nBowling for Soup\nAlan Walker\n"),])
+                         [("-f", "2", "-d", ",", "fourchords.csv", "Artist\nMatt Redman\xa0and\xa0Jonas Myrin\nThirsty Merc\nHarry Styles\nToto\nCheb Khaled\nMichel Teló\nMichelle Branch\nBowling for Soup\nAlan Walker\n"),
+                          ("-f", '"1 2"', "-d", ",", "fourchords.csv", '\ufeffSong title\tArtist\n"10000 Reasons (Bless the Lord)"\tMatt Redman\xa0and\xa0Jonas Myrin\n"20 Good Reasons"\tThirsty Merc\n"Adore You"\tHarry Styles\n"Africa"\tToto\n"Aicha"\tCheb Khaled\n"Ai Se Eu Te Pego"\tMichel Teló\n"All You Wanted"\tMichelle Branch\n"Almost"\tBowling for Soup\n"Alone"\tAlan Walker\n')])
 def test_delimiter_option(field_option, field_range, delimiter_option, delimiter, file_name, expected_result):
     result = os.popen(' '.join(['cccut', field_option, field_range, delimiter_option, delimiter, file_name, '|', 'head'])).read()
     assert expected_result == result
